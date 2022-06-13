389ba02cf57200ce7870c53fcb08f6887ec00b3f
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-06-13 17:42:16 -0400

    added html to display API data, created display function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ var apiKey = "7c0b6aaecdbe620bab99fac8b7710966"
 
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#cityname");
+var currentContainerEl = document.querySelector("#current-container")
+var citySearchTerm = document.querySelector("#city-search-term")
+var currentDateEl = document.querySelector("#city-current-date")
+var currentIconEl = document.querySelector("#city-current-icon")
 
 
 //Function that will execute upon form submission
@@ -31,9 +35,29 @@ var getCityWeather = function(city) {
    //make a request to url
    fetch(currentWeatherUrl).then(function(response) {
     response.json().then(function(data) {
-        console.log(data);
+        
+        // diplay city data to the console 
+        displayCityWeather(data, city)
+        
     })
    })
+    
+}
+
+// Function to display current weather for city
+var displayCityWeather = function (city, searchTerm) {
+    console.log(city);
+    console.log(searchTerm);
+
+    // clear old content
+    currentContainerEl.textContent = "";
+    citySearchTerm.textContent = searchTerm;
+
+    //display current weather data 
+    var displayCurrentDate = document.querySelector("#city-current-date")
+    var currentDate = moment();
+    displayCurrentDate.textContent = currentDate.format("(L)");
+
     
 }
 
