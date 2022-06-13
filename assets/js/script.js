f5023937cdef4df0a1c7e11df56d0e6ff9246c97
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-06-12 22:48:31 -0400

    Test out new logic with parameter/ test was successful

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,19 @@
 var apiKey = "7c0b6aaecdbe620bab99fac8b7710966"
 
 
-var getCityWeather = function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Newark&appid=" + apiKey);
-  };
+var getCityWeather = function(city) {
+    // format the openweather api
+   var currentWeatherUrl= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-  getCityWeather();
+   //make a request to url
+   fetch(currentWeatherUrl).then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+    })
+   })
+    
+}
+
+  getCityWeather("Edison");
   
   
