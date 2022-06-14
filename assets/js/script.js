56579f27b8b013548de5fc48277e08eeb6026e0b
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-06-14 00:32:03 -0400

    saved search to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,11 @@ var currentContainerEl = document.querySelector("#current-container")
 var citySearchTerm = document.querySelector("#city-search-term")
 var currentDateEl = document.querySelector("#city-current-date")
 var currentIconEl = document.querySelector("#city-current-icon")
+var fiveDayEl = document.querySelector("#forecast-cards");
+var searchContainerEl = document.getElementById("search-container");
 var currentUvEl = document.querySelector("#uv-input")
+
+var cityArray = [];
 
 
 //Function that will execute upon form submission
@@ -20,13 +24,40 @@ var formSubmitHandler = function(event) {
 
     if(cityname) {
         getCityWeather(cityname);
+        getForecast(cityname)
+
+        // Push city name into Array in Local Storage
+        cityArray.push(cityname);
+        localStorage.setItem("cityname", JSON.stringify(cityArray));
+        localStorage.getItem("citname");
+
+        document.getElementById("search-container").textContent = cityArray;
+        
+
+        // Empty City Search Bar
         cityInputEl.value= "";
+        
     } else {
         alert("Please enter city name")
     }
 
-    console.log(event);
+    //Set local Storage to search value text
+  localStorage.setItem('searchText', cityname)
+
+ // If local storage is full run populate with saved city text. (for reload)
+if(searchTerm !== null){
+    console.log(searchTerm)
+    var text = searchTerm
+    populate(text)
+  }
+
+  
+    
+  console.log(event);
 };
+
+
+
 
 
 var getCityWeather = function(city) {
@@ -127,6 +158,89 @@ var displayCurrentUv = function(data) {
             currentUvEl.innerHTML=" " + uv + " ";
         }
 };
+
+// 5 day forecast API 
+var getForecast = function(city) {
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&cnt=6&appid=" + apiKey;
+
+    // if response was successful 
+    fetch(forecastURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayForecast(data.list);
+            });
+        } else {
+            alert("Error:" + response.statusText);
+        }
+    })
+    // if network error 
+    .catch(function(error) {
+        alert("Unable to connect to Open Weather");
+    })
+};
+
+// Displaying 5 day forecast   
+var displayForecast = function (list) { 
+    console.log(list);
+
+        for (var i = 0; i <= 4; i++) {
+
+        //date
+        var displayDate1 = document.querySelector("#date-0");
+        var forecastDate1 = moment().add(1, "days").format("L");
+        displayDate1.textContent = forecastDate1;
+
+        var displayDate2 = document.querySelector("#date-1");
+        var forecastDate2 = moment().add(2, "days").format("L");
+        displayDate2.textContent = forecastDate2;
+
+        var displayDate3 = document.querySelector("#date-2");
+        var forecastDate3 = moment().add(3, "days").format("L");
+        displayDate3.textContent = forecastDate3;
+
+        var displayDate4 = document.querySelector("#date-3");
+        var forecastDate4 = moment().add(4, "days").format("L");
+        displayDate4.textContent = forecastDate4;
+
+        var displayDate5 = document.querySelector("#date-4");
+        var forecastDate5 = moment().add(5, "days").format("L");
+        displayDate5.textContent = forecastDate5;
+
+        // temp
+        var displayTemp = document.querySelector(`#temp-${i}`);
+        var forecastTemp = list[i].main.temp + " °F";
+        displayTemp.textContent = forecastTemp; 
+
+        //humidity
+        var displayHumidity = document.querySelector(`#humidity-${i}`);
+        var forecastHumidity = list[i].main.humidity + "%";
+        displayHumidity.textContent = forecastHumidity;
+        
+        // weather icons 
+        var displayIcon1 = document.querySelector("#city-icon-1");
+        var currentIcon1 = "https://openweathermap.org/img/wn/" + list[1].weather[0].icon + "@2x.png"
+        displayIcon1.setAttribute ("src", currentIcon1);
+
+        var displayIcon2 = document.querySelector("#city-icon-2");
+        var currentIcon2 = "https://openweathermap.org/img/wn/" + list[2].weather[0].icon  + "@2x.png"
+        displayIcon2.setAttribute ("src", currentIcon2);
+
+        var displayIcon3 = document.querySelector("#city-icon-3");
+        var currentIcon3 = "https://openweathermap.org/img/wn/" + list[3].weather[0].icon  + "@2x.png"
+        displayIcon3.setAttribute ("src", currentIcon3);
+
+        var displayIcon4 = document.querySelector("#city-icon-4");
+        var currentIcon4 = "https://openweathermap.org/img/wn/" + list[4].weather[0].icon  + "@2x.png"
+        displayIcon4.setAttribute ("src", currentIcon4);
+
+        var displayIcon5 = document.querySelector("#city-icon-5");
+        var currentIcon5 = "https://openweathermap.org/img/wn/" + list[5].weather[0].icon  + "@2x.png"
+        displayIcon5.setAttribute ("src", currentIcon5);
+
+        }
+    }
+
+   
 
   userFormEl.addEventListener("submit", formSubmitHandler);
   
